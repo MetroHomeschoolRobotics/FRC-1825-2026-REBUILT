d9a2e76b47936c654a10caa1c835388c1296f668
Ironclad33 <bricelaycock@gmail.com>
2026-03-14 12:32:39 -0500

    autos more reliable (it works in the sim har har har)

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -1,16 +1,23 @@
 package frc.robot;
 
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
 import choreo.auto.AutoFactory;
 import choreo.auto.AutoRoutine;
 import choreo.auto.AutoTrajectory;
 import choreo.trajectory.Trajectory;
 
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.ChangeTurretMode;
 import frc.robot.commands.DeployIntake;
 import frc.robot.commands.RunFullIndexing;
 import frc.robot.commands.RunIntake;
+import frc.robot.commands.SetInterpolatedShooterRPM;
+import frc.robot.commands.SetShooterRPM;
 import frc.robot.commands.AutoSetInterpolatedShooterRPM;
 
 import frc.robot.subsystems.CommandSwerveDrivetrain;
@@ -43,14 +50,19 @@ public class Autos {
         final AutoTrajectory startToShoot = routine.trajectory("midToShooting");
         final AutoTrajectory shootToDepot = routine.trajectory("midShootingToDepot");
 
-        routine.active().onTrue(
-            startToShoot.resetOdometry()
-            .andThen(new ChangeTurretMode(drivetrain, "Hub"))
-            .andThen(startToShoot.cmd()).deadlineFor(new AutoSetInterpolatedShooterRPM(drivetrain,shooter))
-            .andThen(Commands.waitSeconds(0.3)).deadlineFor(new RunFullIndexing(indexer))
-            .andThen(Commands.waitSeconds(.5)).deadlineFor(new DeployIntake(intake))
-            .andThen(shootToDepot.cmd()).raceWith(new AutoSetInterpolatedShooterRPM(drivetrain,shooter), new SequentialCommandGroup(Commands.waitSeconds(3.6),new RunIntake(intake)))
-            .andThen(Commands.waitSeconds(5)).deadlineFor(new RunFullIndexing(indexer))
+        routine.active().onTrue(startToShoot.resetOdometry()
+        .andThen(startToShoot.cmd())
+        .andThen(new ParallelDeadlineGroup(shootToDepot.cmd(),new SequentialCommandGroup(new DeployIntake(intake),new RunIntake(intake))))
+        .andThen(new SequentialCommandGroup(new ChangeTurretMode(drivetrain, "Hub"),new SetInterpolatedShooterRPM(drivetrain, shooter)))
+        .andThen(new ParallelRaceGroup(new RunFullIndexing(indexer),Commands.waitSeconds(5)))
+            .andThen(new SetShooterRPM(shooter,0))
+            // startToShoot.resetOdometry()
+            // .andThen(new ChangeTurretMode(drivetrain, "Hub"))
+            // .andThen(startToShoot.cmd())//.deadlineFor(new AutoSetInterpolatedShooterRPM(drivetrain,shooter))
+            // .andThen(Commands.waitSeconds(0.3)).deadlineFor(new RunFullIndexing(indexer))
+            // .andThen(Commands.waitSeconds(.5)).deadlineFor(new DeployIntake(intake))
+            // .andThen(shootToDepot.cmd())//.raceWith(new AutoSetInterpolatedShooterRPM(drivetrain,shooter), new SequentialCommandGroup(Commands.waitSeconds(3.6),new RunIntake(intake)))
+            // .andThen(Commands.waitSeconds(5)).deadlineFor(new RunFullIndexing(indexer))
             );
 
 
@@ -60,11 +72,16 @@ public class Autos {
         AutoRoutine routine = factory.newRoutine("leftAuto");
         final AutoTrajectory startToMid = routine.trajectory("leftTrenchToMid");
         final AutoTrajectory midToShoot = routine.trajectory("leftMidToAlliance");
+        final var idle = new SwerveRequest.Idle();
         routine.active().onTrue(
-            startToMid.resetOdometry()
-            .andThen(startToMid.cmd()).deadlineFor(new SequentialCommandGroup(new DeployIntake(intake),new RunIntake(intake)))
-            .andThen(midToShoot.cmd()).deadlineFor(new ChangeTurretMode(drivetrain, "Hub"),new AutoSetInterpolatedShooterRPM(drivetrain, shooter))
-            .andThen(new RunFullIndexing(indexer))
+                  startToMid.resetOdometry().
+            andThen(new ParallelDeadlineGroup(startToMid.cmd(),new SequentialCommandGroup(new DeployIntake(intake),new RunIntake(intake))))
+            .andThen(midToShoot.cmd())
+            .andThen(new SequentialCommandGroup(new ChangeTurretMode(drivetrain, "Hub"),new SetInterpolatedShooterRPM(drivetrain, shooter)))
+            
+            .andThen(new ParallelRaceGroup(new RunFullIndexing(indexer),Commands.waitSeconds(5)))
+            .andThen(new SetShooterRPM(shooter,0))
+
         );
         return routine;
     }
@@ -72,11 +89,21 @@ public class Autos {
         AutoRoutine routine = factory.newRoutine("rightAuto");
         final AutoTrajectory startToMid = routine.trajectory("rightTrenchToMid");
         final AutoTrajectory midToShoot = routine.trajectory("rightMidToAlliance");
+        final var idle = new SwerveRequest.Idle();
         routine.active().onTrue(
-            startToMid.resetOdometry()
-            .andThen(startToMid.cmd()).deadlineFor(new SequentialCommandGroup(new DeployIntake(intake),new RunIntake(intake)))
-            .andThen(midToShoot.cmd()).deadlineFor(new ChangeTurretMode(drivetrain, "Hub"),new AutoSetInterpolatedShooterRPM(drivetrain, shooter))
-            .andThen(new RunFullIndexing(indexer))
+            // startToMid.resetOdometry()
+            // .andThen(startToMid.cmd()).deadlineFor(new SequentialCommandGroup(new DeployIntake(intake),new RunIntake(intake)))
+            // .andThen(midToShoot.cmd())
+            // .andThen(new SequentialCommandGroup(new ChangeTurretMode(drivetrain, "Hub"),new SetInterpolatedShooterRPM(drivetrain, shooter)))
+            // .andThen(drivetrain.applyRequest(() -> idle))
+            // .andThen(new RunFullIndexing(indexer)).raceWith(Commands.waitSeconds(5))
+            // .andThen(new SetShooterRPM(shooter,0))
+            startToMid.resetOdometry().
+            andThen(new ParallelDeadlineGroup(startToMid.cmd(),new SequentialCommandGroup(new DeployIntake(intake),new RunIntake(intake))))
+            .andThen(midToShoot.cmd())
+            .andThen(new SequentialCommandGroup(new ChangeTurretMode(drivetrain, "Hub"),new SetInterpolatedShooterRPM(drivetrain, shooter)))
+            
+            .andThen(new ParallelRaceGroup(new RunFullIndexing(indexer),Commands.waitSeconds(5))).andThen(new SetShooterRPM(shooter,0))
         );
         return routine;
     }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -99,10 +99,10 @@ public class RobotContainer {
             drivetrain.applyRequest(() -> idle).ignoringDisable(true)
         );
 
-        //driverXbox.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        driverXbox.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-driverXbox.getLeftY(), -driverXbox.getLeftX()))
-        ));
+        driverXbox.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        // driverXbox.b().whileTrue(drivetrain.applyRequest(() ->
+        //     point.withModuleDirection(new Rotation2d(-driverXbox.getLeftY(), -driverXbox.getLeftX()))
+        // ));
         
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
